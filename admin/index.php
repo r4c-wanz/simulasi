257757881da9bc1ded4340acf86a3f8184f5d7a9
R4C <103583267+R4C-Wanz@users.noreply.github.com>
2022-11-29 20:22:19 +0700

    admin

---
+++ admin/index.php
@@ -0,0 +1,11 @@
+<?php require '../layout/index.html'?>
+
+<div class="sidebar">
+    <a href="index.php">Dashboard</a>
+    <a href="data_produk.php">Data Produk</a>
+    <a href="">Logout</a>
+</div>
+
+<div class="main">
+    <h2>Hello Welcome to Dashboard</h2>
+</div>
